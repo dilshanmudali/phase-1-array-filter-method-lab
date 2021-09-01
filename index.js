ea0540e91e4ddee6173f4f127a1c3bdc6342c96a
Dilshan <dilshan.mudali@gmail.com>
2021-09-01 10:53:02 -0400

    commit tree a6400764b7442de0d2e6f95395f04f6229ac7fab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+const findMatching = (arr,input) => {
+    return arr.filter(match => match.toLowerCase() === input.toLowerCase())
+}
+
+['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+const fuzzyMatch = (arr,input) => {
+    return arr.filter((elem) => {
+        if(elem.slice(0,1) === input.slice(0,1)){
+            return true
+        }
+    })
+}
+
+const matchName = (arr,input) => {
+    return arr.filter(elem => elem.name === input)
+}
